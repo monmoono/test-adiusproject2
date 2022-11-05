acf265c726793cdda176d6fff3c8d75a7c864a43
monmoono <56115746+monmoono@users.noreply.github.com>
2022-11-05 09:16:42 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -158,6 +158,11 @@ export default {
 
 <style lang="scss">
 @import "@/assets/button.scss";
+body{
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
